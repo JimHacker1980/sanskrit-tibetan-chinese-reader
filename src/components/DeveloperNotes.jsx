fadae5b8331c706dc15a1b008240431ff6a56c9b
Yichen Xu <114908980+JimHacker1980@users.noreply.github.com>
2025-04-28 17:49:24 +0800

    Update DeveloperNotes.jsx

--- src/components/DeveloperNotes.jsx
+++ src/components/DeveloperNotes.jsx
@@ -7,7 +7,7 @@ const DeveloperNotes = () => {
             <p>欢迎使用 Sanskrit-Tibetan-Chinese Reader！</p>
             <p>此项目旨在帮助用户更方便地阅读和翻译梵语、藏语和汉语文本。</p>
             <p>本项目采用了dharmamitra的接口，其中梵语支持语法分析和翻译，其他语言支持翻译</p>
-            <p>本软件目前还只是一个demo，欢迎向开发者提出各种需求！</p>
+            <p>本软件目前还只是一个demo，欢迎在https://github.com/JimHacker1980/sanskrit-tibetan-chinese-reader的issue区向开发者提出各种需求！</p>
         </div>
     );
 };
