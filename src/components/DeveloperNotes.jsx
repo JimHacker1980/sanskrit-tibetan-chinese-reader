e439df8153306e7e1869f814b3ed2b3329fdee79
JimHacker1980 <1024887222@qq.com>
2025-04-30 15:24:48 +0800

    14rd commit

--- src/components/DeveloperNotes.jsx
+++ src/components/DeveloperNotes.jsx
@@ -9,6 +9,7 @@ const DeveloperNotes = () => {
             <p>本项目采用了dharmamitra的接口，其中梵语支持语法分析和翻译，其他语言支持翻译。</p>
             <p>本网页与dharmamitra.org的区别在于，本网页旨在提供一个梵藏汉翻译的平台，可以服务于研究。</p>
             <p>这体现在可以导入导出文件，保留每次结果。这是像我这种上梵语阅读课的人必备的。</p>
+            <p>本网页的使用教程见https://www.bilibili.com/video/BV1RBGCzbEvM</p>
             <p>欢迎在https://github.com/JimHacker1980/sanskrit-tibetan-chinese-reader的issue区向开发者提出各种需求！</p>
         </div>
     );
